d8f0ce1a08048caf8332ac589584d2181180c8f0
Bongju <bonjwow@gmail.com>
2021-04-06 22:43:21 -0400

    Edit data cleaning code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## About The Project
 
-In this research paper, I am going to examine some of the factors that may impact job interest of new coders (beginner programmers) in the technology field. I am going to mainly focus on how educational background, such as the level of education and the field of study, plays a role in deciding what kind of job beginner programmers are looking for.
+In this research paper, I examine some of the factors that may impact job interest of new coders (beginner programmers) in the technology field. I mainly focus on how educational background, such as the level of education and the field of study, plays a role in deciding what kind of job beginner programmers are looking for.
 
 The dataset used for this paper is the survey collected by `freeCodeCamp` in 2017 [Link](https://github.com/freeCodeCamp/2017-new-coder-survey). 
 
--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -8,10 +8,70 @@
 #### Workspace setup ####
 library(haven)
 library(tidyverse)
-# Read in the raw data. 
+library(dplyr)
+
+#### Get data ####
 raw_data <- readr::read_csv("https://raw.githubusercontent.com/freeCodeCamp/2017-new-coder-survey/ed8a2c5118209fa26cc823fd33fedcf6fe5661ec/clean-data/2017-fCC-New-Coders-Survey-Data.csv")
 
-         
+#### Clean data ####
+dfClean <-
+  raw_data %>%
+  select('Age',
+         'CityPopulation',
+         'CommuteTime',
+         'Gender',
+         'Income',
+         'MoneyForLearning',
+         'MonthsProgramming',
+         'SchoolDegree') %>%
+  ### Recode
+  mutate(CityPopulation = dplyr::recode(CityPopulation,
+                                     "less than 100,000" = 0,
+                                     "between 100,000 and 1 million" = 1,
+                                     "more than 1 million" = 2,
+                                     .default = 999)) %>%
+  mutate(CommuteTime = dplyr::recode(CommuteTime,
+                                     "I work from home" = 0,
+                                     "Less than 15 minutes" = 1,
+                                     "15 to 29 minutes" = 2,
+                                     "30 to 44 minutes" = 3,
+                                     "45 to 60 minutes" = 4,
+                                     "More than 60 minutes" = 5,
+                                     .default = 999)) %>%
+  mutate(Gender = dplyr::recode(Gender,
+                                "male" = 0,
+                                "female" = 1,
+                                .default = 999)) %>%
+  mutate(SchoolDegree = dplyr::recode(SchoolDegree,
+                                "no high school (secondary school)" = 0,
+                                "some high school" = 0,
+                                "high school diploma or equivalent (GED)" = 0,
+                                "trade, technical, or vocational training" = 0,
+                                "some college credit, no degree" = 1,
+                                "associate's degree" = 1,
+                                "bachelor's degree" = 2,
+                                "master's degree (non-professional)" = 3,
+                                "professional degree (MBA, MD, JD, etc.)" = 3,
+                                "Ph.D." = 4,
+                                .default = 999)) %>%
+  ### Remove empty rows
+  mutate_all(~ifelse(. %in% c(999, "N/A", "null", ""), NA, .)) %>%
+  na.omit()
+
+#### Check re-coded values ####
+# unique(dfClean$CityPopulation)
+# unique(dfClean$CommuteTime)
+# unique(dfClean$Gender)
+# unique(dfClean$SchoolDegree)
+
+#### Print summary ####
+summary(dfClean)
+
+#### Print structure ####
+str(dfClean)
+
+#### Save cleaned data ####
+write_csv(dfClean, "inputs/data/clean_new-coders.csv")
 
 
 
